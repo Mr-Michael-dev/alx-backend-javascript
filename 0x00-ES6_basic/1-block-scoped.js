298c30ef2bae24e91f4ac6897fb6430134bdbea1
Mr-Michael-dev <michael.oyedepo@gmail.com>
2024-06-25 16:56:22 +0100

    the wonders of spread operator

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,12 +1,12 @@
 // modify the variables inside the function taskBlock
 // so that the variables aren’t overwritten inside the conditional block
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    let task = true;
-    let task2 = false;
+    const task = true;
+    const task2 = false;
   }
 
   return [task, task2];
