ba36dc8a082c2f9f86873545c8524b6549926bc5
Mr_Michael_dev <michael.oyedepo@gmail.com>
2024-06-25 04:59:20 +0000

    Block Scope

--- 0x00-ES6_basic/0-main.js
+++ 0x00-ES6_basic/0-main.js
@@ -1,5 +1,5 @@
 //  a test for 0-constants
 
-import { taskFirst, taskNext } from './0-constants.js';
+import { taskFirst, taskNext } from './0-constants';
 
 console.log(`${taskFirst()} ${taskNext()}`);
