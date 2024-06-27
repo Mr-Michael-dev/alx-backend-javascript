cb754a3727df20ee9400b065a98c43afd08e7fff
Mr_Michael_dev <michael.oyedepo@gmail.com>
2024-06-27 04:02:21 +0000

    Handle multiple successful promises

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,11 +1,14 @@
 // Return a Promise
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => {
+  promise.then(resolve => {
     console.log('Got a response from the API');
     return (
       {
         status: 200,
         body: 'Success',
       });
-  }).catch(() => (Error()));
+  }).catch(reject => {
+    console.log('Got a response from the API');
+    return new Error()
+  });
 }
---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,16 @@
+/* 
+ * utils.js return promise
+ * collectively resolve all promises and
+ * log body firstName lastName to the console.
+*/
+import { uploadPhoto, createUser } from "./utils";
+
+export default function handleProfileSignup() {
+  Promise.all([uploadPhoto(), createUser()]).then(responses => {
+    const [photoResponse, userResponse] = responses;
+    console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
+  }).catch(error => {
+    console.log('Signup system offline');
+    return new Error(error);
+  });
+}
