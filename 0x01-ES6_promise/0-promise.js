03e692c48e9fcc2b68a7a88be6972d6552921c45
Mr-Michael-dev <michael.oyedepo@gmail.com>
2024-06-27 03:57:18 +0100

    return a promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,13 +1,4 @@
 // Return a Promise
 export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    setTimeout( () => {
-      const success = true;
-    if (success) {
-      resolve('resolved');
-    } else {
-      reject(Error('An error occurred'));
-    }
-  }, 1500);
-});
+  return new Promise((resolve, reject) => {});
 }
---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,13 @@
+// Return a Promise
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
+    } else {
+      reject(Error('The fake API is not working currently'));
+    }
+  });
+}
