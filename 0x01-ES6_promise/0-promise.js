993cd8463d8e873bbb57e4b544654065cfe556ce
Mr-Michael-dev <michael.oyedepo@gmail.com>
2024-06-27 04:09:39 +0100

    catch an error

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,4 +1,13 @@
 // Return a Promise
 export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {});
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const success = true;
+      if (success) {
+        resolve('resolved');
+      } else {
+        reject(Error('An error occurred'));
+      }
+    }, 1500);
+  });
 }
