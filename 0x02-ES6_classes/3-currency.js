56aeb3afeeed53398362820eb14297406fd61e8e
Mr-Michael-dev <michael.oyedepo@gmail.com>
2024-06-28 04:57:36 +0100

    create Currency class

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,45 +1,44 @@
 // Implement a class named Currency
 
 export default class HolbertonCourse {
-    constructor(code, name) {
-      if (typeof code !== 'string') {
-        throw new TypeError('Code must be a string');
-      }
-      if (typeof name !== 'string') {
-        throw new TypeError('Name must be a string');
-      }
-
-      this._code = code;
-      this._name = name;
+  constructor(code, name) {
+    if (typeof code !== 'string') {
+      throw new TypeError('Code must be a string');
     }
-  
-    // Getters
-    get code() {
-      return this._code;
-    }
-  
-    get name() {
-      return this._name;
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
     }
 
-    // Setters
-    set code(value) {
-      if (typeof value !== 'string') {
-        throw new TypeError('Code must be a string');
-      }
-      this._code = value;
-    }
-  
-    set name(value) {
-      if (typeof value !== 'string') {
-        throw new TypeError('Name must be a string');
-      }
-      this._name = value;
-    }
-
-    // displayFullCurrency method
-    displayFullCurrency() {
-      return `${this.name} (${this.code})`;
-    }
+    this._code = code;
+    this._name = name;
   }
-  
+
+  // Getters
+  get code() {
+    return this._code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  // Setters
+  set code(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+    this._code = value;
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = value;
+  }
+
+  // displayFullCurrency method
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`;
+  }
+}
