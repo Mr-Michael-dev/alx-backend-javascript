71780ea787c8a8f40866f5e52aa3f0163b5ecf3c
Mr-Michael-dev <michael.oyedepo@gmail.com>
2024-07-03 12:38:12 +0100

    map data structure

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,13 @@
+//  returns a map of groceries
+
+export default function groceriesList() {
+  return new Map(
+    [
+      ['Apples', 10],
+      ['Tomatoes', 10],
+      ['Pasta', 1],
+      ['Rice', 1],
+      ['Banana', 5],
+    ],
+  );
+}
