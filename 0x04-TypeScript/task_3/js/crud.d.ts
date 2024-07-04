a3b0b486eb9d891d86d9eead4773e36d26afbbcc
Mr-Michael-dev <michael.oyedepo@gmail.com>
2024-07-04 19:59:38 +0100

    create ambient namespaces

---
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -0,0 +1,8 @@
+// containing the type declarations for each crud function 
+import {RowID, RowElement} from './interface';
+
+declare module './crud' {
+    export function insertRow(row: RowElement): RowID;
+    export function deleteRow(rowId: RowID): void;
+    export function updateRow(rowId: RowID, row: RowElement): RowID;
+}
