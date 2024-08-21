9290b94cfff3b9719a8779982debc34266708084
Mr-Michael-dev <michael.oyedepo@gmail.com>
2024-08-21 19:34:57 +0100

    Create a small HTTP server using Express

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,15 @@
+// create a small HTTP server using Express module
+
+const express = require('express');
+
+const app = express();
+const port = 1245;
+
+app.get('/', (req, res) => {
+  res.statusCode = 200
+  res.send('Hello Holberton School!');
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
