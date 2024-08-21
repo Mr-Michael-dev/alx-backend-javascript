cf95d0a308b11a0e34342ce14702d80838c00423
Mr-Michael-dev <michael.oyedepo@gmail.com>
2024-08-21 19:55:53 +0100

    Create a more complex HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -4,33 +4,31 @@ const http = require('http');
 const countStudents = require('./3-read_file_async');
 
 const port = 1245;
-const app = http.createServer(async(req, res) => {
-    path = req.url
-    res.setHeader('Content-Type', 'text/plain');
+const app = http.createServer(async (req, res) => {
+  const path = req.url;
+  res.setHeader('Content-Type', 'text/plain');
 
-    if (path === '/') {
+  if (path === '/') {
+    res.statusCode = 200;
+    res.end('Hello Holberton School!');
+  }
+  if (path === '/students') {
+    const db = process.argv[2];
+
+    try {
+      let studentData = '';
+
+      console.log = (message) => { studentData += `${message}\n`; };
+
+      await countStudents(db);
       res.statusCode = 200;
-      res.end('Hello Holberton School!');
+      res.end(`This is the list of our students\n${studentData.trim()}`);
+    } catch (error) {
+      res.statusCode = 500;
+      res.end('Cannot load the database');
     }
-
-    if (path === '/students') {
-      db = process.argv[2]
-      
-      try{
-        let studentData = '';
-
-        console.log = (message) => {studentData += message + '\n';}
-        
-        await countStudents(db)
-        res.statusCode = 200;
-        res.end(`This is the list of our students\n${studentData.trim()}`);
-      } catch (error) {
-        res.statusCode = 500
-        res.end('Cannot load the database');
-      }
-    }
-  },
-);
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
