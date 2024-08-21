5e2e53c61dff2919a4f8a3241c5c6acaa5f38b81
Mr-Michael-dev <michael.oyedepo@gmail.com>
2024-08-21 19:56:34 +0100

    Create a more complex HTTP server using Express

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,33 @@
+// create a small HTTP server using Express module
+
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+
+const app = express();
+const port = 1245;
+
+app.get('/', (req, res) => {
+  res.statusCode = 200;
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', async (req, res) => {
+  const db = process.argv[2];
+
+  try {
+    let studentData = '';
+
+    console.log = (message) => { studentData += `${message}\n`; };
+
+    await countStudents(db);
+    res.statusCode = 200;
+    res.end(`This is the list of our students\n${studentData.trim()}`);
+  } catch (error) {
+    res.statusCode = 500;
+    res.end('Cannot load the database');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
