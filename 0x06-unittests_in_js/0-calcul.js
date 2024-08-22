bcb2cea59f00deee5434bcef6bb99af34ff1cd2b
Mr-Michael-dev <michael.oyedepo@gmail.com>
2024-08-22 20:11:52 +0100

    Basic test with Mocha and Node assertion library

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,6 +1,9 @@
 // round the two arguments and return the sum
 
 function calculateNumber(a, b) {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error();
+  }
   const roundedA = Math.round(a);
   const roundedB = Math.round(b);
   return roundedA + roundedB;
--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -12,6 +12,20 @@ describe('calculateNumber', () => {
     assert.strictEqual(calculateNumber(4, 5.8), 10);
   });
 
+  it('should handle negative numbers', () => {
+    assert.strictEqual(calculateNumber(-2, 3), 1);
+    assert.strictEqual(calculateNumber(-4, -5), -9);
+  });
+
+  it('should throw error when non integers are passed', () => {
+    assert.throws(calculateNumber.bind(null, 'e', 3.7), Error);
+    assert.throws(calculateNumber.bind(null, 2.5, 'b'), Error);
+  });
+
+  it('should handle zero', () => {
+    assert.strictEqual(calculateNumber(0, 0), 0);
+  });
+
   it('should handle decimal numbers', () => {
     assert.strictEqual(calculateNumber(2.5, 3.7), 7);
     assert.strictEqual(calculateNumber(4.2, 5.8), 10);
