918859c791d1b2cda4410eeb1d6fcd5ae5b86fce
Mr-Michael-dev <michael.oyedepo@gmail.com>
2024-08-23 21:51:24 +0100

    Basic Integration testing

---
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -0,0 +1,19 @@
+// integration tests for express app
+const request = require('request');
+const { expect } = require('chai');
+
+describe('gET /', () => {
+  it('should return a response code of 200', () => new Promise((done) => {
+    request.get('http://127.0.0.1:7865/', (error, response) => {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  }));
+
+  it('should return the right body', () => new Promise((done) => {
+    request.get('http://127.0.0.1:7865/', (error, response, body) => {
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  }));
+});
