e1cd564f28529d5e649b06f681efdb908a163267
Mr-Michael-dev <michael.oyedepo@gmail.com>
2024-08-23 05:57:55 +0100

    debug linting error

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -4,20 +4,20 @@ const { expect } = require('chai');
 const Utils = require('./utils');
 const sendPaymentRequestToApi = require('./3-payment');
 
-describe('sendPaymentRequestToApi', function() {
+describe('sendPaymentRequestToApi', () => {
   let mySpy;
 
-  beforeEach(function() {
+  beforeEach(() => {
     mySpy = spy(Utils, 'calculateNumber');
   });
 
-  afterEach(function() {
+  afterEach(() => {
     mySpy.restore();
   });
 
-  it('should call Utils.calculateNumber with SUM and the correct arguments', function() {
+  it('should call Utils.calculateNumber with SUM and the correct arguments', () => {
     sendPaymentRequestToApi(100, 20);
-    
+
     expect(mySpy.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
   });
 });
